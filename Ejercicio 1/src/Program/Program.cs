1d23ca6ec2a417a0e9a5501b9425c6350dd9750f
Felipe Villaronga <110826870+FelipeVillaronga@users.noreply.github.com>
2022-08-29 10:43:33 -0300

    Update Program.cs

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -10,8 +10,9 @@ namespace SRP
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
             ShelveBook shelve1= new ShelveBook (book1, "A","7"); 
-            ShelveBook shelve1= new ShelveBook (book2, "B","3");
+            ShelveBook shelve2= new ShelveBook (book2, "B","3");
         }
     }
     //Tras haber creado una nueva clase para la estantería, se deben crear nuevas estancias para cada "lugar", y asociar cada lugar al libro que le corresponda
+    //Para ello tambien fue necesario modificar el Libro.cs para que pida como parametro las instancias book creadas.
 }
