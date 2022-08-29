058ef68d1b88dc18a08a5bf957713a67b99800b8
Felipe Villaronga <110826870+FelipeVillaronga@users.noreply.github.com>
2022-08-29 14:39:19 -0300

    Update Libro.cs
    ShelveBooks no debe estar incluido como parametro dentro de la clase Book, sino como una nueva clase, la cual a su vez sera creada en un nuevo archivo. Esto es porque de lo contrario no se respetaría el primer principio SOLID (SRP) el cual enuncia que una clase debe tener una única razón para ser modificada; si se quisiera crear un nuevo atributo a la clase ShelveBooks (ej: Edificio, Piso) significaria una segunda razon para cambiar la clase Books (del mismo modo que con ShelveBooks, puede surgir la necesidad de craer un nuevo atributo -ej: Año-).

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -1,17 +1,9 @@
-﻿using System;
+using System;
 
 namespace SRP
 {
     public class Book
     {
-
-        public string Title { get ; }
-        public string Author { get ; }
-        public string Code { get ;  }
-      /*public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
-      */
-
         public Book(String title, String author, String code)
         {
             this.Title = title;
@@ -19,27 +11,19 @@ namespace SRP
             this.Code = code;
         }
 
-      /*public void ShelveBook(String sector, String shelve)
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
-        }
-        */
-    }
-    public class ShelveBook
-    {
-        public Book book { get; }
-        //se debe llamar a la clase Book para poder acceder a la instancia que esta cree, y poder asociarla al sector y shelve fijado.
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
+        public string Title { get ; }
+        public string Author { get ; }
+        public string Code { get ; }
         
-        public void ShelveBook(Book book, String sector, String shelve)
+      /*public string LibrarySector { get ; set; }
+        public string LibraryShelve { get ; set; }
+
+        public void ShelveBook(String sector, String shelve)
         {
-            this.book = book;
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
+      */
+
     }
-    // La clase Book, tenía dos constructores, y una clase puede tener un único método de clase. Asimismo, de surgir la necesidad de modificar el metodo ShelveBook (crear una nueva variable "edificio" o "departamento") se crearia una segunda razon para modificar la clase Book; lo cual contradice el principio SRP.
-    //Por ello es necesario crear una nueva clase -ShelveBook- en la cual, ahora sí, crear un método constructor para los atributos LibrarySector y LibraryShelve
 }
