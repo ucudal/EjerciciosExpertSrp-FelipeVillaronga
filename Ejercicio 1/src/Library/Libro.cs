0ce87d32b7117adf669725aa9f56fa3a67c8c216
Felipe Villaronga <110826870+FelipeVillaronga@users.noreply.github.com>
2022-08-29 10:29:26 -0300

    Update Libro.cs

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -8,8 +8,9 @@ namespace SRP
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
-        public string LibrarySector { get ; set; }
+      /*public string LibrarySector { get ; set; }
         public string LibraryShelve { get ; set; }
+      */
 
         public Book(String title, String author, String code)
         {
@@ -18,11 +19,24 @@ namespace SRP
             this.Code = code;
         }
 
+      /*public void ShelveBook(String sector, String shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }
+        */
+    }
+    public class ShelveBook
+    {
+        public string LibrarySector { get ; set; }
+        public string LibraryShelve { get ; set; }
+        
         public void ShelveBook(String sector, String shelve)
         {
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
-
     }
+    // La clase Book, tenía dos constructores, y una clase puede tener un único método de clase.
+    //Por ello es necesario crear una nueva clase -ShelveBook- en la cual, ahora sí, crear un método constructor para los atributos LibrarySector y LibraryShelve
 }
