b809f25badfc6c2249202302cd7700697fb8f47d
Felipe Villaronga <110826870+FelipeVillaronga@users.noreply.github.com>
2022-09-02 14:14:02 -0300

    Update ShelveBooks.cs

--- Ejercicio 1/src/Library/ShelveBooks.cs
+++ Ejercicio 1/src/Library/ShelveBooks.cs
@@ -3,15 +3,19 @@ using System;
 namespace SRP;
    public class ShelveBook
     {
-        public Book book { get; }
-        //se debe llamar a la clase Book para poder acceder a la instancia que esta cree, y poder asociarla al sector y shelve fijado.
         public string LibrarySector { get ; }
         public string LibraryShelve { get ;  }
-        public  ShelveBook(Book book, String sector, String shelve)
+        public  ShelveBook(String sector, String shelve)
         {
-            this.book = book;
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
         
+        public string Printer2()
+        {
+            return $"Sector: {LibrarySector}. \nEstanteria: {LibraryShelve}.";
+        }
+        
     }
+    // La clase Book, tenía dos constructores, y una clase puede tener un único método de clase. Asimismo, de surgir la necesidad de modificar el metodo ShelveBook (crear una nueva variable "edificio" o "departamento") se crearia una segunda razon para modificar la clase Book; lo cual contradice el principio SRP.
+    //Por ello es necesario crear una nueva clase -ShelveBook- en la cual, ahora sí, crear un método constructor para los atributos LibrarySector y LibraryShelve
