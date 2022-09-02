495e528bb859941533eb54c3bcb7c1f532ef44dc
Felipe Villaronga <110826870+FelipeVillaronga@users.noreply.github.com>
2022-09-02 14:19:26 -0300

    Update Program.cs

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -12,7 +12,14 @@ namespace SRP
             Book book2 = new Book("Pro C#","Troelsen","001-035");
             ShelveBook shelve1= new ShelveBook (book1, "A","7"); 
             ShelveBook shelve2= new ShelveBook (book2, "B","3");
+            BookAndShelve bookShelve1= new BookAndShelve (book1, shelve1);
+            BookAndShelve bookShelve2= new BookAndShelve (book2, shelve2);
         }
-    
+        public void Printer ()
+        {
+            Console.WriteLine(book1.BookPrinter());
+            Console.WriteLine(shelve1.BookShelvePrinter());
+            Console.WriteLine(book2.BookPrinter());
+            Console.WriteLine(shelve2.BookShelvePrinter());
     }
 }
