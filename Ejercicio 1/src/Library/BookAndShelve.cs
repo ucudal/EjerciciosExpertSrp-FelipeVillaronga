71ea7571c687a9bc1d206739e2700d1c8bda2c8f
Felipe Villaronga <110826870+FelipeVillaronga@users.noreply.github.com>
2022-09-02 14:16:59 -0300

    Create BookAndShelve.cs

---
+++ Ejercicio 1/src/Library/BookAndShelve.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace SRP;
+
+class BookAndShelve
+{
+  private Book book;
+  private ShelveBook shelve;
+  
+  public BookAndShelve()
+  {
+    this.book= book;
+    this.shelve= shelve;
+  }
+}
